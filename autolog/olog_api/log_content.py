f56ecb102f53b014a9d00b81137dd03b4139e78d
antoine choquet <antoine.choquet@cea.fr>
2026-02-11 12:55:02 +0100

    refactor: log context management

--- autolog/olog_api/log_content.py
+++ autolog/olog_api/log_content.py
@@ -2,17 +2,16 @@
 import uuid
 import json
 import logging
+import os
 from autolog.cac import caget, is_connected
 
 def define_body(username: str, trigger_pv_name: str, log_info: dict, autolog_context: dict):
     """
     Build API request body with log information
     """
-    if autolog_context == {}:
-        context_desc = ""
-    else:
-        context_desc = create_context_desc(trigger_pv_name, autolog_context)
+    context_desc = create_context_desc(trigger_pv_name, autolog_context)
     main_desc = f"{log_info['description']}\n\n" + context_desc
+
     log_entry =  {
                    "owner": f"{username}",
                    "description": f"{main_desc}",
@@ -38,36 +37,39 @@ def define_body(username: str, trigger_pv_name: str, log_info: dict, autolog_con
 
 def get_more_info(autolog_context: dict):
     """
-    Get more info with PV provided as key "info_pv_name" into TOML file. 
+    Get more info with PV provided as key "info_pv_name" in TOML file.
     """
-    context_pv = autolog_context['pv']
-    as_string = context_pv['as_string']
-    more_info = ""
+    more_info = "\n\n[Context]\n\n"
+
     if 'description' in autolog_context:
-        autolog_desc = autolog_context['description']
-        more_info = more_info + f"\n\n {autolog_desc} \n\n"
-    if not context_pv.get("info_pv_name"):
+        more_info += f"\n\n{autolog_context['description']}\n\n"
+
+    if 'pv' not in autolog_context or not autolog_context['pv'].get("info_pv_name"):
         return more_info
+
+    context_pv = autolog_context['pv']
     pv_name = context_pv['info_pv_name']
+
     if not is_connected(pv_name):
-        more_info = more_info + "Not connected"
+        more_info += "Not connected"
         return more_info
+
     info_pv_value = caget(pv_name)
-    info_pv_value_as_string = caget(pv_name, as_string=True)
+
+    as_string = context_pv['as_string']
     if as_string == "yes":
-        more_info = more_info +\
-                    f"- {info_pv_value},\n\n" + \
-                    f"- {info_pv_value_as_string}"
+        info_pv_value_as_string = caget(pv_name, as_string=True)
+        more_info += f"- {info_pv_value},\n\n- {info_pv_value_as_string}"
     elif as_string == "only":
-        more_info = more_info +\
-                    f"- {info_pv_value_as_string}"
-    elif as_string == "no":
-        more_info = more_info +\
-                    f"- {info_pv_value}"
-    desc = context_pv['info_pv_desc']
-    if desc:
+        info_pv_value_as_string = caget(pv_name, as_string=True)
+        more_info += f"- {info_pv_value_as_string}"
+    else:
+        more_info += f"- {info_pv_value}"
+
+    if context_pv.get('info_pv_desc', False):
         desc_pv = caget(f"{pv_name}.DESC")
-        more_info = more_info + f"\n\n - [PV_DESC] : {desc_pv} \n\n"
+        more_info += f"\n\n- [PV_DESC]: {desc_pv}\n\n"
+    more_info += f"\n\n- [PV_NAME]: {pv_name}\n\n"
     return more_info
 
 def manage_attachment_file(log_entry: dict, file_path: str):
@@ -97,16 +99,14 @@ def create_context_desc(trigger_pv_name: str, autolog_context: dict):
     """
     Create the description part of the log entry
     """
-    more_info = "\n\n [Context] \n\n"
-    for index, context in enumerate(autolog_context):
-        if context.get('pv'):
+    more_info = ""
+    if autolog_context != {}:
+        for index, context in enumerate(autolog_context):
             more_info = more_info + get_more_info(context)
-        else:
-            more_info = more_info + ""
 
     pv_actual_value = caget(trigger_pv_name)
     context_desc =\
-        f"\n\nThe log creation has been triggered by the pv: {trigger_pv_name}, \
+        f"\n\nThe log creation has been triggered by the PV: {trigger_pv_name}, \
             with value: {pv_actual_value}" \
         + more_info \
         + "\n\n Log created automatically by the application AutOlog"
